f8bd72b8695e45dcde445d949c432972fe08cc9b
Rica <rica@zup.ee>
2020-12-09 14:02:08 +0300

    first rect

---
+++ index.js
@@ -0,0 +1,12 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect(l, b){
+    console.log("Solving for rectangle with l = " +l +" b= "+ b);
+}
+
+solveRect(2, 3);
+solveRect(20, 32);
+solveRect(21, 33);
