fb9bbd15ee19e2dfcb86723db02e6bc5aafd7d7b
Rica <rica@zup.ee>
2020-12-09 18:07:14 +0300

    ok revert 2

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 const http = require('http');
 
-<<<<<<< HEAD
-const hostname = 'localhost';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    console.log(req.headers);
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello World</h1></body></html>');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
-});
-=======
 function solveRect(l, b){
     console.log("Solving for rectangle with l = " +l +" b= "+ b);
 }
@@ -24,4 +8,3 @@ solveRect(2, 3);
 solveRect(20, 32);
 solveRect(21, 33);
 solveRect(21, 33);
->>>>>>> parent of 421bb94...  node callback
