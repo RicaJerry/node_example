421bb940912c2becdd2f17d96114029923862dbc
Rica <rica@zup.ee>
2020-12-09 17:40:58 +0300

    node callback

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ var rect = require('./rectangle')
 
 function solveRect(l, b){
     console.log("Solving for rectangle with l = " +l +" b= "+ b);
-}
 
+    rect(l, b, (err, rectangle) => {
+        if(err){
+            console.error("ERROR:", err.message);
+        }else{
+            console.log("Perimeter l = " + l + " b = " + b + " perimeter :" + rectangle.perimeter());
+            console.log("Area l = " + l + " b = " + b + " area :" + rectangle.area(l+1, b+1));
+        }
+    });
+
+    console.log("this statement is after the call to rect ");
+}
 solveRect(2, 3);
 solveRect(20, 32);
-solveRect(21, 33);
+solveRect(-21, 33);
 solveRect(21, 33);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,12 @@
-exports.perimeter =  (x,y) => (2*(x+y));
-module.exports.area =  (x,y) => (x*y)
+module.exports = (x, y, callback) => {
+    if(x <= 0 || y <= 0){
+        setTimeout(() => callback(new Error("value x = " + x + " y = " + y +  " should be greater than 0 "), null),
+            2000);
+    }else{
+        setTimeout(() => callback(null, 
+            {
+                perimeter : () => (2*(x+y)),
+                area :  (x,y) => (x*y)
+            }), 2000)
+    }
+}
