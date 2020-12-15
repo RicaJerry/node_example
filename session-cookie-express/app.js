20bca33928c45457cf1477864d74f26b895d6f65
Rica <rica@zup.ee>
2020-12-15 09:45:08 +0300

    basic auth with cookie

--- session-cookie-express/app.js
+++ session-cookie-express/app.js
@@ -51,36 +51,26 @@ app.use (
   })
 );
 
+
+app.use ('/', indexRouter);
+app.use ('/users', usersRouter);
+
 function auth (req, res, next) {
   console.log (req.headers);
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-
-    if (!authHeader) {
       var err = new Error ('Not authorized ! XDD Lol !!! ');
 
       res.setHeader ('WWW-Authenticate', 'Basic');
       err.status = 401;
       return next (err);
-    }
-
-    var auth = new Buffer.from (authHeader.split (' ')[1], 'base64')
-      .toString ()
-      .split (':');
-
-      var username = auth[0];
-      var password = auth[1];
-    if (username === 'admin' && password === 'password') {
-      req.session.user = 'admin';
-      next ();
-    }
   } else {
-    if (req.session.user === 'admin') {
+    console.log("LL: auth -> req.session.user", req.session.user)
+
+    if (req.session.user === 'authenticated') {
       next ();
     } else {
       var err = new Error (' You are not authorized !!! huhu ');
-
-      err.status = 401;
+      err.status = 403;
       return next (err);
     }
   }
@@ -88,8 +78,6 @@ function auth (req, res, next) {
 
 app.use (auth);
 
-app.use ('/', indexRouter);
-app.use ('/users', usersRouter);
 app.use ('/dishes', dishRouter);
 
 // catch 404 and forward to error handler
--- session-cookie-express/routes/users.js
+++ session-cookie-express/routes/users.js
@@ -27,6 +27,66 @@ router.post('/signup', function(req, res, next) {
     res.json({status: "Yosh ! Registration Successful", user: user});
   }, (err) => next(err))
   .catch((err) => next(err))
-})
+});
+
+router.post('/login', function(req, res, next){
+  if (!req.session.user) {
+    var authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      var err = new Error ('Not authorized ! XDD Lol !!! ');
+
+      res.setHeader ('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next (err);
+    }
+
+    var auth = new Buffer.from (authHeader.split (' ')[1], 'base64')
+      .toString ()
+      .split (':');
+
+      var username = auth[0];
+      var password = auth[1];
+
+      User.findOne({username: username})
+      .then((user) => {
+        if(user === null){
+          var err = new Error(' Oups !!' + username + " does not exist ");
+          err.status = 403;
+          return next(err);
+        }
+        else if(user.password !== password){
+          var err = new Error(' Oups !!' + password + " is incorect ");
+          err.status = 403;
+          return next(err);
+        }
+        else if (user.username ===  username && user.password === password) {
+          req.session.user = 'authenticated';
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/plain");
+          res.end('Yoshhh !!! You are authenticated');
+        }
+      })
+      .catch((err) => next(err));
+  }
+  else{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('You are already authenticated');
+  }
+});
+
+router.get('/logout', (req, res) => {
+  if(req.session){
+    req.session.destroy();
+    res.clearCookie('session-id');
+    res.redirect('/');
+  }
+  else{
+    var err = new Error('You are not logged in !!! ');
+    err.status = 403;
+    next(err);
+  }
+});
 
 module.exports = router;
