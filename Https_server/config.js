228899345f082a18faaf8b2825339b87b63aa5fe
Rica <rica@zup.ee>
2021-01-19 23:50:12 +0300

    add

---
+++ Https_server/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secretKey': 'itsjustasecretkey',
+    'mongoUrl': 'mongodb+srv://github:githubpassword@mongodb-github.vn7kz.mongodb.net/mongoDB-github?retryWrites=true&w=majority',
+}
---
+++ Https_server/routes/dishRouter.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const authenticate = require('../authenticated');
+
+const Dishes = require('../models/dishes');
+const disRouter = express.Router();
+
+disRouter.use(bodyParser.json());
+
+disRouter.route('/')
+.get((req, res, next) => {
+    Dishes.find({})
+    .then((dishes)=> {
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dishes);          
+    }, (err) => next(err))
+    .catch((err) => next(err))
+})
+.post(authenticate.verifyUser, (req, res, next)=>{
+    Dishes.create(req.body)
+    .then((dish) => {
+        console.log('Dish created');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'applicaiton/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err))
+
+})
+
+module.exports = disRouter;
